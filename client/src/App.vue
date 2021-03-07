89efe77f01435a603d3c45502cc86db2e84d2602
limguoquan <limguoquan@gmail.com>
2021-03-07 16:09:32 +0800

    initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="vue-template">
+    <!-- Navigation -->
+    <nav class="navbar shadow bg-white rounded justify-content-end fixed-top">
+      <div class="container">
+        <a class="navbar-brand float-left">
+           10 Degrees
+        </a>
+        <ul class="nav navbar-nav flex-row float-right">
+          <li class="nav-item">
+            <router-link class="nav-link pr-3" to="/">Sign in</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="btn btn-outline-primary" to="/Signup">Sign up</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- Main -->
+    <div class="App">
+      <div class="vertical-center">
+          <router-view />
+      </div>
+    </div>
+  </div>
+</template>
