a0d70ddb11f5afb6acc295e21d8116183de919bc
Zhiying15 <zhiying15@hotmail.com>
2021-04-07 15:13:15 +0800

    edited login.vue

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,37 +1,35 @@
 <template>
-  <b-modal ref="login-modal" hide-footer>
+<b-modal ref="login-modal" hide-footer>
     <div class="modal-body">
-      <form @submit.prevent="loginUser">
-        <h3>Sign In</h3>
+        <form @submit.prevent="loginUser">
+            <h3>Sign In</h3>
 
-        <div class="form-group">
-          <label>Email Address</label>
-          <input
-            type="email"
-            class="form-control form-control-lg"
-            v-model="login.email"
-          />
-        </div>
+            <div class="form-group">
+                <label>Email Address</label>
+                <input
+                    type="email"
+                    class="form-control form-control-lg" 
+                    v-model="login.email"
+                />
+            </div>
 
-        <div class="form-group">
-          <label>Password</label>
-          <input
-            type="password"
-            class="form-control form-control-lg"
-            v-model="login.password"
-          />
-        </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input
+                    type="password"
+                    class="form-control form-control-lg"
+                    v-model="login.password"
+                />
+            </div>
 
-        <b-button 
-        type="submit" 
-        variant="info"
-        @click="loginUser">
-          <p1 style="color: white">Sign In</p1>
-        </b-button>
-        <!-- on click, call method "login" -->
-      </form>
-    </div>
-  </b-modal>
+            <b-button
+                type="submit"
+                variant = "info"
+            ><p1 style = "color:white">Sign In</p1>
+            </b-button> <!-- on click, call method "login" -->
+        </form>
+    </div>    
+</b-modal>
 </template>
 
 <script>
@@ -58,11 +56,10 @@
                     this.$swal("Error", "Invalid login credentials", "error")
                     console.log(err.response)
                 }
-      }
-    },
-    showModal() {
-      this.$refs["login-modal"].show();
-    },
-
-};
+            },
+            showModal(){
+                this.$refs['login-modal'].show();
+            }
+        }
+    }
 </script>
