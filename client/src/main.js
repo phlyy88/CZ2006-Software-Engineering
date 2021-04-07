c66d4056a4c33f0461b78f27f45ff43f3ccba4ab
haozhi98 <57322708+haozhi98@users.noreply.github.com>
2021-04-07 15:51:44 +0800

    notification

--- client/src/main.js
+++ client/src/main.js
@@ -5,6 +5,7 @@ import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import Notifications from 'vue-notification'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -18,13 +19,13 @@ import 'vue-mdi/dist/mdi.css';
 
 import { GridPlugin } from "@syncfusion/ej2-vue-grids"
 import { DropDownListPlugin } from '@syncfusion/ej2-vue-dropdowns'
-import vuetify from './plugins/vuetify';
-import { VueMdi, library } from 'vue-mdi'
-import { mdiAccount } from '@mdi/js'
+// import vuetify from './plugins/vuetify';
+// import { VueMdi, library } from 'vue-mdi'
+// import { mdiAccount } from '@mdi/js'
 
-library.add({ mdiAccount })
+// library.add({ mdiAccount })
  
-Vue.component('vue-mdi', VueMdi)
+// Vue.component('vue-mdi', VueMdi)
  
 Vue.config.productionTip = false
 
@@ -35,6 +36,7 @@ Vue.config.productionTip = false
 Vue.use(GridPlugin)
 Vue.use(DropDownListPlugin)
 Vue.use(VueSweetalert2)
+Vue.use(Notifications)
 
 const base = axios.create({
   baseURL: "http://localhost:8080/api"
@@ -45,6 +47,5 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  vuetify,
   render: h => h(App)
 }).$mount('#app')
