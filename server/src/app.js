293c27d4d88be214337ef02c185cfa96eaa287f3
haozhi98 <57322708+haozhi98@users.noreply.github.com>
2021-04-11 13:16:46 +0800

    -

--- server/src/app.js
+++ server/src/app.js
@@ -6,13 +6,6 @@ const config = require("./config/db.config")
 
 const app = express();
 
-
-// var corsOptions = {
-//   origin: "http://localhost:8081"
-// };
-
-// app.use(cors(corsOptions));
-
 app.use(cors())
 app.use(morgan('combined'))
 // parse requests of content-type - application/json
@@ -20,22 +13,6 @@ app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
-// const db = require("./models");
-// db.mongoose
-//   .connect(db.url, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   })
-//   .then(() => {
-//     console.log("Connected to the database!");
-//   })
-//   .catch(err => {
-//     console.log("Cannot connect to the database!", err);
-//     process.exit();
-//   });
 
 const mongoose = require("mongoose");
 mongoose.set("useCreateIndex", true);
@@ -57,9 +34,6 @@ app.use('/api/vehicle', vehicleRoutes)
 const housingRoutes = require('./routes/housing')
 app.use('/api/housing', housingRoutes)
 
-// require('./routes/user')(app)
-
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
